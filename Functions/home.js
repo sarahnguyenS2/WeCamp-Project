1f18f71ea40c02b38e965f016b96471f2c4478b4
Diem Nguyen <nguyentranhoaidiem@gmail.com>
2023-04-06 17:35:06 +0700

    Update product and login

--- Functions/home.js
+++ Functions/home.js
@@ -1,17 +1,16 @@
-const loginButton = document.querySelector("#login");
-// const currentUrl = (window.location.href).split("/").pop();
-// console.log(currentUrl)
-// export function currentUrl() {
-//     const current = (window.location.href).split("/").pop();
-//     return current
-// }
+// const logButton = document.querySelector("#login");
+// // const currentUrl = (window.location.href).split("/").pop();
+// // console.log(currentUrl)
+// // export function currentUrl() {
+// //     const current = (window.location.href).split("/").pop();
+// //     return current
+// // }
 
-// const user = localStorage.getItem(email);
-// const data = JSON.parse(user);
-//  localStorage.setItem("key","value")
-//     const data = localStorage.getItem(key)
-//     console.log(data)
-loginButton.addEventListener("click", () => {
-    window.location.assign("login.html")
-})
-
+// // const user = localStorage.getItem(email);
+// // const data = JSON.parse(user);
+// //  localStorage.setItem("key","value")
+// //     const data = localStorage.getItem(key)
+// //     console.log(data)
+// logButton.addEventListener("click", () => {
+//   window.location.assign("login.html");
+// });
